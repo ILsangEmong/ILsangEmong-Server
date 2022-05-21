e9e20e50b914ac6479a5838bd5909e046cacdc1f
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-22 02:05:10 +0900

    [FEAT] Team Router 연결

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 //router index file
 import { Router } from 'express';
-
+import TeamRouter from './TeamRouter';
 
 const router = Router();
 
-
+router.use('/team', TeamRouter);
 
 export default router;
