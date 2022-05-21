fc05b0e053806d5ec5dbe46eb08a1c013e545cb4
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-22 04:53:57 +0900

    [CHORE] responseMessage 수정

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -5,11 +5,8 @@ const message = {
     BAD_REQUEST: '잘못된 요청',
     INTERNAL_SERVER_ERROR: '서버 내부 오류',
 
-    // 팀
     CREATE_TEAM_SUCCESS: '팀 생성 성공',
     READ_RELAY_CONTENT_SUCCESS: '릴레이 콘텐츠 결과 조회 성공',
-
-    // 코멘트 추가
     UPDATE_COMMENT_SUCCESS: '코멘트가 저장되었습니다',
 };
 
