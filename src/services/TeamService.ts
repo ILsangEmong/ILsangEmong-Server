9ed198992893014aa9d7376c63de03c44648d282
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-22 02:03:06 +0900

    [FEAT] TeamService 생성, 팀 생성 service 로직 구현

---
+++ src/services/TeamService.ts
@@ -0,0 +1,29 @@
+import { InviteCodeDto } from '../interfaces/team/InviteCodeDto';
+import { TeamCreateDto } from '../interfaces/team/TeamCreateDto';
+import Team from '../models/Team';
+import { randomString } from '../modules/random';
+
+const createTeam = async (
+    teamCreateDto: TeamCreateDto
+): Promise<InviteCodeDto> => {
+    const inviteCode = randomString();
+    try {
+        teamCreateDto.inviteCode = inviteCode;
+        const team = new Team(teamCreateDto);
+
+        await team.save();
+
+        const data = {
+            inviteCode: inviteCode,
+        };
+
+        return data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export default {
+    createTeam,
+};
