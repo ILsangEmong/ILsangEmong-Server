1ce0ffe81f823c9d6fd1872d372985b30eba5bea
Hyosik Philip Joo <lux1937@gmail.com>
2022-05-22 04:55:38 +0900

    [FEAT] 코멘트 결과 확인 API 라우터 연결

--- src/routes/CommentRouter.ts
+++ src/routes/CommentRouter.ts
@@ -4,5 +4,6 @@ import CommentController from '../controllers/CommentController';
 const router: Router = Router();
 
 router.put('/', CommentController.updateComment);
+router.get('/:inviteCode', CommentController.getComments);
 
 export default router;
