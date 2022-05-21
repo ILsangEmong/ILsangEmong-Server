32a88ae073b929ebdbae9530c7ac72de3f208a29
thguss <thgus345@naver.com>
2022-05-22 02:38:25 +0900

    [ADD] 코멘트 저장 message 추가

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -3,6 +3,9 @@ const message = {
     NOT_FOUND: '존재하지 않는 자원',
     BAD_REQUEST: '잘못된 요청',
     INTERNAL_SERVER_ERROR: '서버 내부 오류',
+
+    // 코멘트 추가
+    UPDATE_COMMENT_SUCCESS: '코멘트가 저장되었습니다',
 };
 
 export default message;
